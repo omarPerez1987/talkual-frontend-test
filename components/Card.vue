7d32f3161e2eec01824a070e035a5e1ddfcfac49
omarPerez1987 <omar.perez.dev@gmail.com>
2024-03-25 17:45:03 +0100

    feat: añado funcionalidad al componente Card para si el pedido ha sido donado cambia el boton a Realizar envio

--- components/Card.vue
+++ components/Card.vue
@@ -1,17 +1,19 @@
 <script setup>
 defineProps(['order']);
+
 </script>
 <template>
     <article class="card-body">
         <h5 class="card-title">SKU: {{ order.attributes.order_items.data[0].attributes.sku }}</h5>
         <p class="card-text">Type: {{ order.attributes.type }}</p>
         <p class="card-text">Quantity: {{ order.attributes.order_items.data[0].attributes.quantity }}</p>
-        <p class="card-text">Shipping Firstname: {{ order.attributes.order_meta.data.attributes.shipping_firstname }}</p>
-        <NuxtLink v-if="order.attributes.type !== 'donation'" class="btn btn-primary" :to="`/donation/${order.id}`">
-            Donate
+        <p class="card-text">Shipping Firstname: {{ order.attributes.order_meta.data.attributes.shipping_firstname }}
+        </p>
+        <NuxtLink v-if="order.attributes.status !== 'cancelled'"
+            class="btn btn-primary me-3" :to="`/donation/${order.id}`">
+            {{ order.attributes.type === 'donation' ? 'Realizar envio' : 'Donate' }}
         </NuxtLink>
     </article>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
